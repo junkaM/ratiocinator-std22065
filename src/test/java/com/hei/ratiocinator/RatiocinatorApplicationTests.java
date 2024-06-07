d5f59dc31d6b1a3a8ec7faf49c262346ff97a534
junkaM <hei.mikanto.3@gmail.com>
2024-06-07 12:27:31 +0200

    initialisation du projet

---
+++ src/test/java/com/hei/ratiocinator/RatiocinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.ratiocinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatiocinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
